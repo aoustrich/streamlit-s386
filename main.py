efaf4b58920557fcfbd728d755f33089bfd24210
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-11-27 19:24:50 -0700

    Personalized App

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import plotly.express as px
 
-st.title('My first app!')
+st.title('Popular Names')
 url = "https://raw.githubusercontent.com/esnt/Data/main/Names/popular_names.csv"
 df = pd.read_csv(url)
 
-selected_name = st.text_input("Enter a name", "Frodo")
+selected_name = st.text_input("Enter a name to see its popularity over time", "ex: John")
 
 name_df = df[df['name'] == selected_name]
 if name_df.empty:
-    st.write("No data found")
+    st.write("No data found. Check your spelling and try again.")
 else:
     fig = px.line(name_df, x="year", y="n", color='sex',
                   color_discrete_sequence=['#F8766D', '#00BA38'])
     st.plotly_chart(fig)
-    
-selected_year = st.selectbox("Please Select a Year", df['year'].unique())
+
+st.markdown("---")
+
+selected_year = st.selectbox("Select a year to its most popular boy and girl names!", df['year'].unique())
 
 year_df = df[df['year'] == selected_year]
 girl_names = year_df[year_df['sex']=='F'].sort_values(by='n', ascending=False).head(5)['name'].reset_index(drop=True)    
 boy_names = year_df[year_df['sex']=='M'].sort_values(by='n', ascending=False).head(5)['name'].reset_index(drop=True)    
 top_names = pd.concat([girl_names, boy_names], axis=1).reset_index(drop=True)
 top_names.columns = ['girl','boy']
-st.write(f"Top names in {selected_year}")
+st.write(f"The top names in {selected_year} are:")
 st.dataframe(top_names)
 
 # import pandas as pd
